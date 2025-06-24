122a529668c0f9c2c02e0ce5c5c1ab6cb991114c
Rahul Krishna <rahulkrish321123@gmail.com>
2025-06-24 11:19:51 +0530

    Added headline text

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,8 @@ import { toast } from "sonner";
 import { useState } from "react";
 import CTA from "@/components/cta";
 import Form from "@/components/form";
-import Logos from "@/components/logos";
 import Particles from "@/components/ui/particles";
 import Header from "@/components/header";
-import Footer from "@/components/footer";
 import { addToWaitlist } from "@/lib/firebase";
 
 export default function Home() {
@@ -126,7 +124,7 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center overflow-x-clip pt-12 md:pt-24">
+    <main className="flex min-h-screen flex-col items-center overflow-x-clip pt-12 md:pt-24 pb-12">
       <section className="flex flex-col items-center px-4 sm:px-6 lg:px-8">
         <Header />
 
@@ -144,7 +142,7 @@ export default function Home() {
       
       </section>
 
-      <Footer />
+
 
       <Particles
         quantityDesktop={350}
