d533f9512db9c33436c14353698a36c1dd53a8b2
Rahul Krishna <rahulkrish321123@gmail.com>
2025-08-10 00:17:11 +0530

    changes og image name

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
-      <meta property="og:image" content="/opengraph-image.png" />
+      <meta property="og:image" content="/opengraph-image2.png" />
       <meta property="og:image:type" content="image/png" />
       <meta property="og:image:width" content="1280" />
       <meta property="og:image:height" content="832" />
@@ -30,7 +30,7 @@ export default function RootLayout({
         property="og:url"
         content="https://getritely.com/"
       />
-      <meta name="twitter:image" content="/opengraph-image.png" />
+      <meta name="twitter:image" content="/opengraph-image2.png" />
       <meta name="twitter:image:type" content="image/png" />
       <meta name="twitter:image:width" content="1280" />
       <meta name="twitter:image:height" content="832" />
